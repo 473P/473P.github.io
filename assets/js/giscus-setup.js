1de72a480a0e4f2515b5ec040af6862e061503db
473P <51024056+473P@users.noreply.github.com>
2025-12-09 01:31:27 +0000

    Deploying to gh-pages from @ 473P/473P.github.io@2232fab175a0344ce6036173a425b3062860aa81 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "473P/Muqiao.github.io",
+    "data-repo": "473P/473P.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
